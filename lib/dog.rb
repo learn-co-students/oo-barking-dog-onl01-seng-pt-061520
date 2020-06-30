fad3b961d592aa5af299d3a48ac5ae2b5b764583
“Megan <meganschertz@gmail.com>
2020-06-30 12:16:27 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    def name
+        @this_dogs_name
+    end
+    def bark=(woof)
+        @this_dog_bark = woof
+    end
+    def bark
+        @this_dog_bark
+        puts "woof!"
+    end
+end
+
+
+
