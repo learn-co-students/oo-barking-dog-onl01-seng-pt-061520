b07f788e4e8b50d677de469b7c9bcbfcd8d18389
Antonio O Fong <omarquinzan@yahoo.com>
2020-07-01 15:25:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+ class Dog
+
+  def name=(this_dog)
+    @this_dog_name = this_dog
+  end
+  def name
+    @this_dog_name
+    
+  end
+  def bark
+    puts "woof!"
+  end
+  binding.pry
+end
+    fido = Dog.new
+    fido.name = "Fido"
+    fido.name
+    fido.bark
+  
+    
+    
+  
+ 
