eb78a9a4ff2874302b8f70c0a5f4fd776fda029d
ejixxer999 <lehman.ethan2@gmail.com>
2020-07-06 19:48:29 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog 
+    def name=(new_dog)
+        @this_dog = new_dog
+    end
+    
+    def name 
+        @this_dog 
+    end 
+
+    def bark
+        puts "woof!"
+    end 
+end 
+
+
+
